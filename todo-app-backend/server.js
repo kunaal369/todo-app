27a8b513d779ad8742fa261bbd509aefd4cf8fbd
Kunaal Rewatkar <kunaalrewatkar5@gmail.com>
2025-06-22 15:56:26 +0530

    Added cors for both local and live

--- todo-app-backend/server.js
+++ todo-app-backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://13.201.1.210:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
