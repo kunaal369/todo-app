3417ca3cb00f7c26f5c6d4e8c7db67b6057a3575
Kunaal Rewatkar <kunaalrewatkar5@gmail.com>
2025-06-22 16:07:23 +0530

    solved cors policy error

--- todo-app-backend/server.js
+++ todo-app-backend/server.js
@@ -9,7 +9,6 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(cors({
     origin: ["http://localhost:3000", "http://13.201.1.210:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
